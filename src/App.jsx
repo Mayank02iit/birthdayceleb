acb20d559ba20397f1acf0dfb0558a89eafa302f
mayank <ee23b045@smail.iitm.ac.in>
2024-12-22 14:16:56 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Particle from "./components/Particle";
+import Content from "./Content";
+
+function App() {
+  return (
+    <div className="font-raleway">
+      <Content />
+      <Particle />
+    </div>
+  );
+}
+
+export default App;
